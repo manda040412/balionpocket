5045b15c175f537e326d6376c719c0814c07bd49
Julius Kayne <99462565+HB-JK@users.noreply.github.com>
2026-01-06 10:37:36 +0700

    update url

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
 export const config = {
-    baseUrl: "http://127.0.0.1:8000",
-    // baseUrl : "https://app-bali-corner.sawmany.com",
+    // baseUrl: "http://127.0.0.1:8000",
+    baseUrl: "https://app-bali-corner.sawmany.com",
 }
